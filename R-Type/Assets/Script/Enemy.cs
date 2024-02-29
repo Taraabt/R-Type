e512f691b6cfcd92a378818d89747f567bcc7e51
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-02-29 15:28:56 +0100

    .

---
+++ R-Type/Assets/Script/Enemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Bullet bullet;
+    [SerializeField] PlayerMovement player;
+    [SerializeField] int totalHp;
+    int currentHp;
+
+
+
+    public void CheckCollision(Collision2D collision){
+        if (collision.gameObject == player.gameObject){
+            player.playerHp = player.playerHp - 3;
+        }
+        else if (collision.gameObject == bullet.gameObject)
+        {
+            currentHp--;
+        }
+    }
+
+
+    private void Start()
+    {
+        currentHp = totalHp;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckCollision(collision);
+    }
+
+    private void Update()
+    {
+        if(currentHp==0) {
+            Destroy(transform.gameObject);
+        }
+        CheckOut();
+    }
+
+    public void CheckOut()
+    {
+        if (transform.position.x <= player.transform.position.x - 20f)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
